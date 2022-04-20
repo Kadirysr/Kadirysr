dbaddb3e46443ce89cee521b50428af135fbed4d
Kadir Yaşar <50399681+Kadirysr@users.noreply.github.com>
2022-04-21 00:32:51 +0300

    Add files via upload
    - Feature Engineering

---
+++ Week_6_Diyabet_Feature_Engineering.py
@@ -0,0 +1,368 @@
+# Gerekli Kütüphane ve Fonksiyonlar
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, roc_auc_score
+from sklearn.model_selection import GridSearchCV, cross_validate
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import StandardScaler
+import warnings
+
+warnings.simplefilter(action="ignore")
+
+df = pd.read_csv("datasets/diabetes.csv")
+df.head()
+
+
+##################################
+# Adım 1: Genel resmi inceleyiniz.
+##################################
+def check_df(dataframe, head=5):
+    print("##################### Shape #####################")
+    print(dataframe.shape)
+    print("##################### Types #####################")
+    print(dataframe.dtypes)
+    print("##################### Head #####################")
+    print(dataframe.head(head))
+    print("##################### Tail #####################")
+    print(dataframe.tail(head))
+    print("##################### NA #####################")
+    print(dataframe.isnull().sum())
+    print("##################### Quantiles #####################")
+    print(dataframe.quantile([0, 0.05, 0.50, 0.95, 0.99, 1]).T)
+
+    check_df(df)
+
+
+#  print(dataframe.shape)
+# 768 gözlem birimi, 9 değişken var.
+
+# print(dataframe.isnull().sum())
+# NA kayıtların olmadığı görülüyor.
+
+
+##################################
+# Adım 2: Numerik ve kategorik değişkenleri yakalayınız.
+##################################
+def grab_col_names(dataframe, cat_th=10, car_th=20):
+    cat_cols = [col for col in dataframe.columns if dataframe[col].dtypes == "O"]
+    num_but_cat = [col for col in dataframe.columns if
+                   dataframe[col].nunique() < cat_th and dataframe[col].dtypes != "O"]
+    cat_but_car = [col for col in dataframe.columns if
+                   dataframe[col].nunique() > car_th and dataframe[col].dtypes == "O"]
+    cat_cols = cat_cols + num_but_cat
+    cat_cols = [col for col in cat_cols if col not in cat_but_car]
+    num_cols = [col for col in dataframe.columns if dataframe[col].dtypes != "O"]
+    num_cols = [col for col in num_cols if col not in num_but_cat]
+    print(f"Observations: {dataframe.shape[0]}")
+    print(f"Variables: {dataframe.shape[1]}")
+    print(f'cat_cols: {len(cat_cols)}')
+    print(f'num_cols: {len(num_cols)}')
+    print(f'cat_but_car: {len(cat_but_car)}')
+    print(f'num_but_cat: {len(num_but_cat)}')
+
+    return cat_cols, num_cols, cat_but_car
+
+
+cat_cols, num_cols, cat_but_car = grab_col_names(df)
+
+# toplam 9 değişken var. 1 categorik değişken, 8 numeric. categoric ama cardelen sayısı 0, extra bilgi olarak numeric but categoric olan ise 1 kayıt imiş.
+
+cat_cols
+num_cols
+cat_but_car
+
+
+##################################
+# KATEGORİK DEĞİŞKENLERİN ANALİZİ
+##################################
+# bağımlı değişken ile yukarıdaki diğer değişkenlerin ne şekilde etki ettiğini öğrenmek için yapıyoruz.
+def cat_summary(dataframe, col_name, plot=False):
+    print(pd.DataFrame({col_name: dataframe[col_name].value_counts(),
+                        "Ratio": 100 * dataframe[col_name].value_counts() / len(dataframe)}))
+    print("##########################################")
+    if plot:
+        sns.countplot(x=dataframe[col_name], data=dataframe)
+        plt.show()
+
+
+cat_summary(df, "Outcome")
+
+
+# categoric değişken olan outcome için 0 ve 1 değerleri var. toplam 500 tane 0 olan, 268 tane 1 olan veri var demektir. %65 - 35.
+# bu analize şekliyle çok sayıda değişkeni tek tek inceleyerek az sayıda sınıfa ait değişkenler tespit edilebilir. örneğin 1 kaydı var ise bu bize bir anlam ifade etmez. silebiliriz.
+# veya rare encoding kullanılabilir.
+
+# NOT: eğer 1 den fazla kategorik değişken var ise for döngüsüne sokabiliriz.
+# for col in cat_cols:
+# cat_summary (df,col)
+
+##################################
+# NUMERİK DEĞİŞKENLERİN ANALİZİ
+##################################
+
+def num_summary(dataframe, numerical_col, plot=False):
+    quantiles = [0.05, 0.10, 0.20, 0.30, 0.40, 0.50, 0.60, 0.70, 0.80, 0.90, 0.95, 0.99]
+    print(dataframe[numerical_col].describe(quantiles).T)
+
+    if plot:
+        dataframe[numerical_col].hist(bins=20)
+        plt.xlabel(numerical_col)
+        plt.title(numerical_col)
+        plt.show()
+
+
+for col in num_cols:
+    num_summary(df, col, plot=True)
+
+
+# plot=True kullanmadan da sadece verileri takip edebiliriz.
+
+
+##################################
+# AYKIRI DEĞER ANALİZİ
+##################################
+
+def outlier_thresholds(dataframe, col_name, q1=0.05, q3=0.95):
+    quartile1 = dataframe[col_name].quantile(q1)
+    quartile3 = dataframe[col_name].quantile(q3)
+    interquantile_range = quartile3 - quartile1
+    up_limit = quartile3 + 1.5 * interquantile_range
+    low_limit = quartile1 - 1.5 * interquantile_range
+    return low_limit, up_limit
+
+
+def check_outlier(dataframe, col_name):
+    low_limit, up_limit = outlier_thresholds(dataframe, col_name)
+    if dataframe[(dataframe[col_name] > up_limit) | (dataframe[col_name] < low_limit)].any(axis=None):
+        return True
+    else:
+        return False
+
+
+def replace_with_thresholds(dataframe, variable, q1=0.05, q3=0.95):
+    low_limit, up_limit = outlier_thresholds(dataframe, variable, q1=0.05, q3=0.95)
+    dataframe.loc[(dataframe[variable] < low_limit), variable] = low_limit
+    dataframe.loc[(dataframe[variable] > up_limit), variable] = up_limit
+
+
+# Aykırı Değer Analizi ve aykırı değerlerden kurtulma
+for col in df.columns:
+    print(col, check_outlier(df, col))
+    if check_outlier(df, col):
+        replace_with_thresholds(df, col)
+
+for col in df.columns:
+    print(col, check_outlier(df, col))
+
+# burada true olanlar için aykırı değer var anlamına gelir.
+# replace_with_thresholds ile bu aykırı değerlerden kurtuluyoruz.
+
+##################################
+# EKSİK DEĞER ANALİZİ
+##################################
+# önce değişken değerlerinde 0 değeri alamayacak değişkenler belirlenir. burada outcome ve pregnaniec dışındakiler 0 değerini alamaz.
+# bu yüzden bu 0 değerlerini null ile değiştiriyoruz.
+zero_columns = [col for col in df.columns if (df[col].min() == 0 and col not in ["Pregnancies", "Outcome"])]
+zero_columns
+for col in zero_columns:
+    df[col] = np.where(df[col] == 0, np.nan, df[col])
+
+# Eksik Gözlem Analizi
+df.isnull().sum()
+
+
+def missing_values_table(dataframe, na_name=False):
+    na_columns = [col for col in dataframe.columns if dataframe[col].isnull().sum() > 0]
+    n_miss = dataframe[na_columns].isnull().sum().sort_values(ascending=False)
+    ratio = (dataframe[na_columns].isnull().sum() / dataframe.shape[0] * 100).sort_values(ascending=False)
+    missing_df = pd.concat([n_miss, np.round(ratio, 2)], axis=1, keys=['n_miss', 'ratio'])
+    print(missing_df, end="\n")
+    if na_name:
+        return na_columns
+
+
+na_columns = missing_values_table(df, na_name=True)
+
+
+# Eksik Değerlerin Bağımlı Değişken ile İlişkisinin İncelenmesi
+def missing_vs_target(dataframe, target, na_columns):
+    temp_df = dataframe.copy()
+    for col in na_columns:
+        temp_df[col + '_NA_FLAG'] = np.where(temp_df[col].isnull(), 1, 0)
+    na_flags = temp_df.loc[:, temp_df.columns.str.contains("_NA_")].columns
+    for col in na_flags:
+        print(pd.DataFrame({"TARGET_MEAN": temp_df.groupby(col)[target].mean(),
+                            "Count": temp_df.groupby(col)[target].count()}), end="\n\n\n")
+
+
+missing_vs_target(df, "Outcome", na_columns)
+
+# Eksik Değerlerin Doldurulması. median ile dolduruyoruz.
+for col in zero_columns:
+    df.loc[df[col].isnull(), col] = df[col].median()
+
+df.isnull().sum()
+
+##################################
+# KORELASYON Analizi
+##################################
+
+# Korelasyon Matrisi
+f, ax = plt.subplots(figsize=[18, 13])
+sns.heatmap(df.corr(), annot=True, fmt=".2f", ax=ax, cmap="magma")
+ax.set_title("Correlation Matrix", fontsize=20)
+plt.show()
+
+df.corr()
+# burada özellikle koyu renkli olanlara odaklanmakta fayda var. bunların ilişkileri korelasyonları kuvvetlidir diyebiliriz.
+
+##################################
+# ÖZELLİK ÇIKARIMI
+##################################
+
+# bundan sonrası tamamen veriyi anlamak ve bu verilerden anlamlı değişkenler elde etmektir. Bu ilerde machine learning için faydalı olacaktır.
+
+# Yaş değişkenini kategorilere ayırıp yeni yaş değişkeni oluşturulması
+# Yaş değişkenini parçalara ayırıyoruz. 50 altı ve üstüne ayırıyoruz.
+df.loc[(df["Age"] >= 21) & (df["Age"] < 50), "NEW_AGE_CAT"] = "mature"
+df.loc[(df["Age"] >= 50), "NEW_AGE_CAT"] = "senior"
+
+# BMI 18,5 aşağısı underweight, 18.5 ile 24.9 arası normal, 24.9 ile 29.9 arası Overweight ve 30 üstü obez
+df['NEW_BMI'] = pd.cut(x=df['BMI'], bins=[0, 18.5, 24.9, 29.9, 100],
+                       labels=["Underweight", "Healthy", "Overweight", "Obese"])
+
+# Glukoz degerini kategorik değişkene çevirme
+df["NEW_GLUCOSE"] = pd.cut(x=df["Glucose"], bins=[0, 140, 200, 300], labels=["Normal", "Prediabetes", "Diabetes"])
+
+# # Yaş ve beden kitle indeksini bir arada düşünerek kategorik değişken oluşturma
+
+df.loc[(df["BMI"] < 18.5) & ((df["Age"] >= 21) & (df["Age"] < 50)), "NEW_AGE_BMI_NOM"] = "underweightmature"
+df.loc[(df["BMI"] < 18.5) & (df["Age"] >= 50), "NEW_AGE_BMI_NOM"] = "underweightsenior"
+df.loc[((df["BMI"] >= 18.5) & (df["BMI"] < 25)) & (
+        (df["Age"] >= 21) & (df["Age"] < 50)), "NEW_AGE_BMI_NOM"] = "healthymature"
+df.loc[((df["BMI"] >= 18.5) & (df["BMI"] < 25)) & (df["Age"] >= 50), "NEW_AGE_BMI_NOM"] = "healthysenior"
+df.loc[((df["BMI"] >= 25) & (df["BMI"] < 30)) & (
+        (df["Age"] >= 21) & (df["Age"] < 50)), "NEW_AGE_BMI_NOM"] = "overweightmature"
+df.loc[((df["BMI"] >= 25) & (df["BMI"] < 30)) & (df["Age"] >= 50), "NEW_AGE_BMI_NOM"] = "overweightsenior"
+df.loc[(df["BMI"] > 18.5) & ((df["Age"] >= 21) & (df["Age"] < 50)), "NEW_AGE_BMI_NOM"] = "obesemature"
+df.loc[(df["BMI"] > 18.5) & (df["Age"] >= 50), "NEW_AGE_BMI_NOM"] = "obesesenior"
+
+# Yaş ve Glikoz değerlerini bir arada düşünerek kategorik değişken oluşturma
+df.loc[(df["Glucose"] < 70) & ((df["Age"] >= 21) & (df["Age"] < 50)), "NEW_AGE_GLUCOSE_NOM"] = "lowmature"
+df.loc[(df["Glucose"] < 70) & (df["Age"] >= 50), "NEW_AGE_GLUCOSE_NOM"] = "lowsenior"
+df.loc[((df["Glucose"] >= 70) & (df["Glucose"] < 100)) & (
+        (df["Age"] >= 21) & (df["Age"] < 50)), "NEW_AGE_GLUCOSE_NOM"] = "normalmature"
+df.loc[((df["Glucose"] >= 70) & (df["Glucose"] < 100)) & (df["Age"] >= 50), "NEW_AGE_GLUCOSE_NOM"] = "normalsenior"
+df.loc[((df["Glucose"] >= 100) & (df["Glucose"] <= 125)) & (
+        (df["Age"] >= 21) & (df["Age"] < 50)), "NEW_AGE_GLUCOSE_NOM"] = "hiddenmature"
+df.loc[((df["Glucose"] >= 100) & (df["Glucose"] <= 125)) & (df["Age"] >= 50), "NEW_AGE_GLUCOSE_NOM"] = "hiddensenior"
+df.loc[(df["Glucose"] > 125) & ((df["Age"] >= 21) & (df["Age"] < 50)), "NEW_AGE_GLUCOSE_NOM"] = "highmature"
+df.loc[(df["Glucose"] > 125) & (df["Age"] >= 50), "NEW_AGE_GLUCOSE_NOM"] = "highsenior"
+
+
+# İnsulin Değeri ile Kategorik değişken türetmek
+def set_insulin(dataframe, col_name="Insulin"):
+    if 16 <= dataframe[col_name] <= 166:
+        return "Normal"
+    else:
+        return "Abnormal"
+
+
+df["NEW_INSULIN_SCORE"] = df.apply(set_insulin, axis=1)
+
+df["NEW_GLUCOSE*INSULIN"] = df["Glucose"] * df["Insulin"]
+df["NEW_GLUCOSE*PREGNANCIES"] = df["Glucose"] * df["Pregnancies"]
+
+# kolonların büyültülmesi
+df.columns = [col.upper() for col in df.columns]
+
+df.head()
+
+##################################
+# ENCODING
+##################################
+
+# Değişkenlerin tiplerine göre ayrılması işlemi
+cat_cols, num_cols, cat_but_car = grab_col_names(df)
+
+
+# LABEL ENCODING
+def label_encoder(dataframe, binary_col):
+    labelencoder = LabelEncoder()
+    dataframe[binary_col] = labelencoder.fit_transform(dataframe[binary_col])
+    return dataframe
+
+
+binary_cols = [col for col in df.columns if df[col].dtypes == "O" and df[col].nunique() == 2]
+binary_cols
+
+for col in binary_cols:
+    df = label_encoder(df, col)
+
+# One-Hot Encoding İşlemi - onehotEncoding – her değişkeni bir feature a çeviriyor. Hepsini 1 ve 0 a dönüştürüyor. Bu sayede değişkenlern tüm modellere adaptasyonu sağlamış olur.
+# cat_cols listesinin güncelleme işlemi
+cat_cols = [col for col in cat_cols if col not in binary_cols and col not in ["OUTCOME"]]
+cat_cols
+
+
+def one_hot_encoder(dataframe, categorical_cols, drop_first=False):
+    dataframe = pd.get_dummies(dataframe, columns=categorical_cols, drop_first=drop_first)
+    return dataframe
+
+
+df = one_hot_encoder(df, cat_cols, drop_first=True)
+
+##################################
+# STANDARTLAŞTIRMA
+##################################
+# standartlaştırma yapıyoruz. Standartlaştırma her zaman tercih edilmeli. Optimizasyonda hata veya sapmaları engellemiş oluyor.
+
+num_cols
+
+scaler = StandardScaler()
+df[num_cols] = scaler.fit_transform(df[num_cols])
+
+df.head()
+df.shape
+
+##################################
+# MODELLEME
+##################################
+
+y = df["OUTCOME"]
+X = df.drop("OUTCOME", axis=1)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=17)
+
+rf_model = RandomForestClassifier(random_state=46).fit(X_train, y_train)
+y_pred = rf_model.predict(X_test)
+
+print(f"Accuracy: {round(accuracy_score(y_pred, y_test), 2)}")
+print(f"Recall: {round(recall_score(y_pred, y_test), 3)}")
+print(f"Precision: {round(precision_score(y_pred, y_test), 2)}")
+print(f"F1: {round(f1_score(y_pred, y_test), 2)}")
+print(f"Auc: {round(roc_auc_score(y_pred, y_test), 2)}")
+
+# Accuracy: Sistemde doğru olarak yapılan tahminlerin tüm tahminlere oranıdır.
+# Accuracy = (TP+TN) / (TP+TN+FN+FP)
+# Recall: Pozitif durumların ne kadar başarılı tahmin edildiğini gösterir.
+# Recall = TP / (TP + FN)
+# Precision: Pozitif olarak tahmin edilen bir durumdaki başarıyı gösteren durum.
+# Precision = TP / (TP + FP)
+# F1: Recall ve Precision’ın harmonik ortalaması.
+
+# Accuracy: 0.79
+# Recall: 0.711
+# Precision: 0.67
+# F1: 0.69
+# Auc: 0.77
+
+# Base Model
+# Accuracy: 0.77
+# Recall: 0.706
+# Precision: 0.59
+# F1: 0.64
+# Auc: 0.75
